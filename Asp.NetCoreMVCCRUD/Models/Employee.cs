d6f7a1a97135bbce22bc101fefd00780b95a9717
VictorJBozzelle <victorjbozzelle@gmail.com>
2019-07-02 04:55:05 -0500

    first commit

---
+++ Asp.NetCoreMVCCRUD/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace Asp.NetCoreMVCCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [DisplayName("Full Name")]
+        [Column(TypeName ="nvarchar(250)")]
+        [Required(ErrorMessage ="This field is required.")]
+        public string FullName { get; set; }
+
+        [DisplayName("Emp. Code")]
+        [Column(TypeName ="varchar(10)")]
+        public string EmpCode { get; set; }
+
+        [Column(TypeName ="varchar(100)")]
+        public string Position { get; set; }
+
+        [DisplayName("Office Location")]
+        [Column(TypeName ="varchar(100)")]
+        public string OfficeLocation { get; set; }
+    }
+}
---
+++ Asp.NetCoreMVCCRUD/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace Asp.NetCoreMVCCRUD
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
